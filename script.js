3e3ff6b19bbfffc8dbf6c0d553e95dd6599ffad3
laurenjacobonii <143565048+laurenjacobonii@users.noreply.github.com>
2025-03-14 13:58:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 const supabaseUrl = 'https://kilhjgwauabvcqabealm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpbGhqZ3dhdWFidmNxYWJlYWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MTQzMTAsImV4cCI6MjA1NzE5MDMxMH0.N12sBlLyI4I6_ewB1hf5SSNKSG6bLMKBrZSpXaGddHQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-
-let { data: Books, error } = await supabase
+async function getBooks() {
+  let { data: Books, error } = await supabase
   .from('Books')
   .select('*')
 
-for (let Book of Books) {
-  let BookList = document.getElementById('Books');
-  BookList.innerHTML += `<li>${Book.Title}</li>`;
+  for (let Book of Books) {
+    let BookList = document.getElementById('Books');
+    BookList.innerHTML += `<li>${Book.Title} - ${Book.Author} - ${Book.ISBN}</li>`;
+  }
 }
+
+getBooks();
